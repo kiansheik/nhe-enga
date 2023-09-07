04ed6a1cc96bf33e633ebbb3273dc453a25aa79e
Kian Sheik <ksheik@referralexchange.com>
2023-09-07 10:01:20 -0300

    fixed dots; less string regex parsing. more trash, but beter to have more content in the beginning; will parse in the future

--- gen_data.py
+++ gen_data.py
@@ -5,34 +5,24 @@ def find_string_in_docx(docx_file):
     doc = Document(docx_file)
     occurrences = []
 
-    lines_per_page = 25  # Adjust this value based on your document's formatting
-
-    current_page = 1
-    lines_count = 0
-
     for para in doc.paragraphs:
         raw_text = para.text.encode("utf-8").decode("utf-8")
         parsed = parse_verbete(raw_text)
         if parsed:
             occurrences.append(parsed)
-        
-        lines_count += 1
-        if lines_count >= lines_per_page:
-            lines_count = 0
-            current_page += 1
 
     return occurrences
 
 def parse_verbete(line):
-    pattern = r"([\w']+?)(\d*)\s+\(([\w\s.]+)\)\s+-\s+(.*)"
+    pattern = r"^([\w']+?)(\d*)\s+(.*)"
 
     match = re.search(pattern, line)
     out = dict()
     if match:
         out['first_word'] = match.group(1)
         out['optional_number'] = match.group(2)
-        out['part_of_speech'] = match.group(3)
-        out['definition'] = match.group(4)
+        out['part_of_speech'] = ""
+        out['definition'] = match.group(3)
         return out
     return None
 
