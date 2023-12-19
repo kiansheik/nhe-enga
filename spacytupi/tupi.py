8f9ace61a1a11c9455bfafe2a86278b833c2902f
Kian Sheik <ksheik@referralexchange.com>
2023-12-19 09:26:13 -0300

    verbo incorpordap

--- spacytupi/tupi.py
+++ spacytupi/tupi.py
@@ -26,28 +26,32 @@ class Verb:
         self.transitive = transitive  # Whether the verb is transitive (boolean)
         self.raw_definition = raw_definition  # Raw definition of the verb (string)
 
-    def conjugate(self, person_tense='1ps', object_tense='3p', mode='indicativo', pos='posposto', pro_drop=False):
+    def conjugate(self, subject_tense='1ps', object_tense='3p', dir_obj_raw=None, mode='indicativo', pos='posposto', pro_drop=False):
         # A simple conjugation function (you can expand this based on language rules)
         if self.verb_class in ["adj.: "]:
-            result = f"{TupiAntigo.personal_inflections[person_tense][1]} {self.verbete}"
+            result = f"{TupiAntigo.personal_inflections[subject_tense][1]} {self.verbete}"
         if self.verb_class in ["(v. intr.)"]:
-            subj = TupiAntigo.personal_inflections[person_tense][0] if not pro_drop else ''
-            result = f"{subj} {TupiAntigo.personal_inflections[person_tense][2]}-{self.verbete}"
+            subj = TupiAntigo.personal_inflections[subject_tense][0] if not pro_drop else ''
+            result = f"{subj} {TupiAntigo.personal_inflections[subject_tense][2]}-{self.verbete}"
         elif self.verb_class in ["(v.tr.)"]:
             if pos not in ['posposto', 'incorporado', 'anteposto']:
                 raise Error("Postposition Not Valid")
             if object_tense in TupiAntigo.personal_inflections.keys():
-                if object_tense == '3p':
-                    subj = TupiAntigo.personal_inflections[person_tense][0] if not pro_drop else ''
-                    pref = TupiAntigo.personal_inflections[person_tense][2]
-                    dir_obj = TupiAntigo.personal_inflections['3p'][0]
+                if object_tense == subject_tense:
+                    subj = TupiAntigo.personal_inflections[subject_tense][1] if not pro_drop else ''
+                    result = f"{subj} {TupiAntigo.personal_inflections[subject_tense][2]}-îe-{self.verbete}"
+                elif object_tense == '3p':
+                    subj = TupiAntigo.personal_inflections[subject_tense][0] if not pro_drop else ''
+                    pref = TupiAntigo.personal_inflections[subject_tense][2]
+                    dir_obj = TupiAntigo.personal_inflections['3p'][0] if dir_obj_raw is None else dir_obj_raw
                     if pos == 'posposto':
                         result = f"{subj} {pref}-î-{self.verbete} {dir_obj}"
                     elif pos == 'anteposto':
                         result = f"{subj} {dir_obj} {pref}-î-{self.verbete}"
                     elif pos == 'incorporado':
-                        result = f"{subj} {pref}-î-{self.verbete}"
-                if object_tense == == 
+                        result = f"{subj} {pref}-{'î' if dir_obj_raw is None else dir_obj}-{self.verbete}"
+                # elif object_tense == '1ps':
+                #     if subject_tense == '1ps'
         else:
             return "Invalid/Unimplemented tense"
         return result.strip()
