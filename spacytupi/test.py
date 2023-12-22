ad85d6a0af40486311090f3d0848c806901a0b82
Marcilio <marcilio.ramos@petrobras.com.br>
2023-12-22 16:10:23 -0300

    aprendendo a usar git

--- spacytupi/test.py
+++ spacytupi/test.py
@@ -6,16 +6,16 @@ print("hello 'ary gûé!")
 verbos = [tupi.Verb("poroîuká","v.","falar"),
           tupi.Verb("porombo'e","v.","falar"),
           tupi.Verb("mba'epe'a","v.","falar"),
-          tupi.Verb("mba'e'u","v.","falar"),
-          tupi.Verb("porabyky","v.","falar"),
-          tupi.Verb("moroekar","v.","falar"),
-          tupi.Verb("mba'eepîak","v.","falar"),
-          tupi.Verb("pab","v.","falar"),
-          tupi.Verb("kanhem","v.","falar"),
-          tupi.Verb("nhan","v.","falar"),
-          tupi.Verb("nhe'eng","v.","falar"),
-          tupi.Verb("manõ","v.","falar"),
-          tupi.Verb("nhe'engĩ","v.","falar")
+          #tupi.Verb("mba'e'u","v.","falar"),
+          #tupi.Verb("porabyky","v.","falar"),
+          #tupi.Verb("moroekar","v.","falar"),
+          #tupi.Verb("mba'eepîak","v.","falar"),
+          #tupi.Verb("pab","v.","falar"),
+          #tupi.Verb("kanhem","v.","falar"),
+          #tupi.Verb("nhan","v.","falar"),
+          #tupi.Verb("nhe'eng","v.","falar"),
+          #tupi.Verb("manõ","v.","falar"),
+          #tupi.Verb("nhe'engĩ","v.","falar")
           ]
 verbo_ero = tupi.Verb("eroîeupir","ero","levar")
 adjetivo = tupi.Verb("oryb","adj (s)","feliz")
