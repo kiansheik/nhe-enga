974930cec63aa14f20e693895db43d94235cd0f3
Nambiguasu <moscomonteiro@hotmail.com>
2023-12-22 10:19:06 -0300

    Create conjugation.py
    Added a function for making and printing to terminal conjugation tables, using a different logic from the one present in the Verb class. I made it an outside function instead of a method so that it doesn't break compatibility with the code Kian is making.

---
+++ spacytupi/conjugation.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+def conjugation_table( verbo_tupi ):
+    pref_intr_df = pd.DataFrame( { '1ps':['a'] ,'1ppi':['îa'],'1ppe':['oro'],
+                                   '2ps':['ere'],'2pp':['pe'] ,'3p':['o'] },
+                                index = ['conjugacao'] )
+    subject_intr_df = pd.DataFrame( { '1ps':['ixé'] ,'1ppi':['îandé'],'1ppe':['oré'],
+                                   '2ps':['endé'],'2pp':['peê'] ,'3p':['a\'e'] },
+                                index = ['conjugacao'] )
+    pref_tr_df = pd.DataFrame( {'1ps': ['aîe' , ''  , ''  , 'oro', 'opo', 'aî'] ,
+                                '1ppi':[''   , 'îaîe', ''  , 'oro', 'opo','îaî'],
+                                '1ppe':[''   , ''  , 'oroîe', 'oro', 'opo','oroî'],
+                                '2ps': ['xe ', 'îandé ','oré ', 'ereîe', ''  ,'ereî'],
+                                '2pp': ['xe ', 'îandé ','oré ', ''  , 'peîe','peî'] ,
+                                '3p':  ['xe ', 'îandé ','oré ', 'nde ', 'pe ','oî'] },
+                                index = ['obj 1ps','obj 1ppi','obj 1ppe',
+                                         'obj 2ps','obj 2pp','obj 3p'] )
+    
+    subject_tr_df = pd.DataFrame( {'1ps': ['îxé','','','îxé','îxé','îxé'] ,
+                                '1ppi':['','îandé','','îandé','îandé','îandé'],
+                                '1ppe':['','','oré','oré','oré','oré'],
+                                '2ps': ['', '','', 'endé', '','endé'],
+                                '2pp': ['', '','', '', 'peê','peê'] ,
+                                '3p':  ['a\'e']*6 },
+                                index = ['obj 1ps','obj 1ppi','obj 1ppe',
+                                         'obj 2ps','obj 2pp','obj 3p'] )
+    
+    pref_tr_plr_df = pd.DataFrame( {'1ps': ['aîe', '', '', 'oro', 'opo', 'as'] ,
+                                '1ppi':['', 'îaîe', '', 'oro', 'opo','îas'],
+                                '1ppe':['', '', 'oroîe', 'oro', 'opo','oros'],
+                                '2ps': ['xe r', 'îandé r','oré r', 'ereîe', '','eres'],
+                                '2pp': ['xe r', 'îandé r','oré r', '', 'peîe','pes'] ,
+                                '3p':  ['xe r', 'îandé r', 'oré r', 'nde r', 'pe r','os'] },
+                                index = ['obj 1ps','obj 1ppi','obj 1ppe',
+                                         'obj 2ps','obj 2pp','obj 3p'] )
+    
+    suf_tr_df =   pd.DataFrame( {'1ps':['', '', '', '', '', ''] ,
+                                '1ppi':['', '', '', '', '', ''],
+                                '1ppe':['', '', '', '', '', ''],
+                                '2ps': ['îepé', 'îepé', 'îepé', '', '', ''],
+                                '2pp': ['peîepé', 'peîepé', 'peîepé', '', '', ''] ,
+                                '3p':  ['', '', '', '', '', ''] },
+                                index = ['obj 1ps','obj 1ppi','obj 1ppe',
+                                         'obj 2ps','obj 2pp','obj 3p'] )
+    
+    second_class_prefix_df = pd.DataFrame( { '1ps':['xe '] ,'1ppi':['îandé '],'1ppe':['oré '],
+                                   '2ps':['nde '],'2pp':['pe '] ,'3p':['i '] },
+                                index = ['conjugacao'] )
+    
+    second_class_plr_prefix_df = pd.DataFrame( { '1ps':['xe r'] ,'1ppi':['îandé r'],'1ppe':['oré r'],
+                                   '2ps':['nde r'],'2pp':['pe r'] ,'3p':['s'] },
+                                index = ['conjugacao'] )
+
+    if verbo_tupi.type == 0:
+        df_table = subject_intr_df +' '+pref_intr_df + verbo_tupi.verbete
+    elif verbo_tupi.type == 1:
+        df_table = subject_tr_df +' '+ pref_tr_df +verbo_tupi.verbete+' '+suf_tr_df
+        df_table = df_table.replace(' '+verbo_tupi.verbete+' ', "--//--")
+    elif verbo_tupi.type == 2:
+        df_table = subject_tr_df +' '+ pref_tr_plr_df + verbo_tupi.verbete+' '+suf_tr_df
+        df_table = df_table.replace(' '+verbo_tupi.verbete+' ', "--//--")
+    elif verbo_tupi.type == 3:
+        df_table = second_class_prefix_df + verbo_tupi.verbete
+    elif verbo_tupi.type == 4:
+        df_table = second_class_plr_prefix_df + verbo_tupi.verbete
+
+    print (df_table)
