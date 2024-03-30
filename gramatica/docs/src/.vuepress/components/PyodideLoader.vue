21a0157400d97dc4631aebd411e3611a01728bf1
Kian Sheik <ksheik@referralexchange.com>
2024-03-30 15:48:59 -0300

    fix the loader

--- gramatica/docs/src/.vuepress/components/PyodideLoader.vue
+++ gramatica/docs/src/.vuepress/components/PyodideLoader.vue
@@ -8,7 +8,7 @@ export default {
     data() {
         return {
             pyodideReady: false,
-            basePath: this.env !== 'production' ? '/' : '/nhe-enga/gramatica/',
+            basePath: this.env === '"development"' ? '/' : '/nhe-enga/gramatica/',
         };
     },
     computed: {
