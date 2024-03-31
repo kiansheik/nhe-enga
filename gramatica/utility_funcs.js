b11e8dd6a3f4342186ddc28ad307eb38f88cbc86
Kian Sheik <ksheik@referralexchange.com>
2024-03-31 02:02:50 -0300

    attempt removing the client only tag

--- gramatica/utility_funcs.js
+++ gramatica/utility_funcs.js
@@ -1,7 +1,11 @@
 function swapTitleAndInnerText(event) {
     const spanElement = event.target.closest('span');
-const oldTitle = spanElement.getAttribute('title');
-const oldInnerText = spanElement.innerText;
-spanElement.setAttribute('title', oldInnerText);
-spanElement.innerText = oldTitle;
+    const oldTitle = spanElement.getAttribute('title');
+    const oldInnerText = spanElement.innerText;
+    spanElement.setAttribute('title', oldInnerText);
+    spanElement.innerText = oldTitle;
+}
+
+function toBoolean(value) {
+    return !['0', 'false', '', 0, undefined, NaN].includes(value);
 }
