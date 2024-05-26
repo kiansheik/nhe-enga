6fae05961adc21a879148f14a43a1f4ab22dcbbd
Kian Sheik <ksheik@referralexchange.com>
2024-05-26 20:59:21 -0300

    first nasal

--- tupi/tupi/noun.py
+++ tupi/tupi/noun.py
@@ -105,6 +105,12 @@ class Noun(TupiAntigo):
             second_last_letter = self.nasal_map.get(start[-2], start[-2])
             first_nasal = self.nasal_prefix_map.get(mod_vbt_an[0], mod_vbt_an[0])
             ret_noun.latest_verbete = f"{start[:-2]}{second_last_letter}[{parts[-1]}{first_nasal}{mod_vbt_an[1:]}"
+        elif ends_with_any(vbt, vogais_nasais):
+            parts = ret_noun.latest_verbete.split("[")
+            start = "[".join(parts[:-1])
+            second_last_letter = self.nasal_map.get(start[-2], start[-2])
+            first_nasal = self.nasal_prefix_map.get(mod_vbt_an[0], mod_vbt_an[0])
+            ret_noun.latest_verbete = f"{start}[{parts[-1]}{first_nasal}{mod_vbt_an[1:]}"
         elif vbt[-1] in self.consoantes:
             parts = ret_noun.latest_verbete.split("[")
             start = "[".join(parts[:-1])
