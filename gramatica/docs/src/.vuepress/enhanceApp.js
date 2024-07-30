58810b13aef5ad7da258b77289691e49b1bb1264
Kian Sheik <ksheik@referralexchange.com>
2024-07-30 11:45:34 -0300

    adding google analytics tags

--- gramatica/docs/src/.vuepress/enhanceApp.js
+++ gramatica/docs/src/.vuepress/enhanceApp.js
@@ -10,5 +10,28 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  // ...apply enhancements for the site.
+  // Google Analytics integration
+  if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+    // Create script tag for Google Analytics
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-DZYY8Y9076';
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'G-DZYY8Y9076');
+
+      // Track page views on route change
+      router.afterEach((to) => {
+        gtag('config', 'G-DZYY8Y9076', { page_path: to.fullPath });
+      });
+    };
+  }
+
+  // ...apply other enhancements for the site.
 }
