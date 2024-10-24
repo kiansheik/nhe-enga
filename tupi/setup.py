5b1907ca438c61614ee8f86e4c5e53ea71ad1e72
Kian Sheik <ksheik@referralexchange.com>
2024-10-24 11:41:27 -0300

    fix broken orthography

--- tupi/setup.py
+++ tupi/setup.py
@@ -4,6 +4,10 @@ setup(
     name='tupi',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,  # Important!
+    package_data={
+        'tupi': ['alt_ort/*'],  # Include all files inside alt_ort
+    },
     description='Description of your package',
     author='Kian Sheik',
     author_email='kiansheik3128@gmail.com',
